3614dde8b06ae4b88b8b9ef3ccf502bb1385fffa
Aniket <rautan_1@rknec.edu>
2023-09-10 22:02:22 +0530

    skills and about me

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 var typed = new Typed(".auto-type", {
-  strings: ["Web Devloper", "Video Editor", "Content Creater"],
+  strings: ["Web Developer", "Video Editor", "Content Creator"], // Fixed a typo in "Developer"
   typeSpeed: 125,
   backSpeed: 125,
   loop: true,
 });
+
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll(".navigation-bar a");
+
+window.onscroll = () => {
+  // Use window.scrollY to get the current scroll position
+  let top = window.scrollY; // Changed from window.screenY
+
+  sections.forEach((sec) => {
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+      });
+
+      // Use document.querySelector to find the navigation link with the corresponding href
+      let link = document.querySelector(
+        ".navigation-bar a[href='#" + id + "']"
+      );
+
+      if (link) {
+        link.classList.add("active");
+      }
+    }
+  });
+};
