a0334413ffe1f4569b8b13962acf2c712ce5fe36
Aniket <rautan_1@rknec.edu>
2023-09-17 22:02:22 +0530

    commit changes

--- script.js
+++ script.js
@@ -7,29 +7,46 @@ var typed = new Typed(".auto-type", {
 
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll(".navigation-bar a");
-
-window.onscroll = () => {
-  // Use window.scrollY to get the current scroll position
-  let top = window.scrollY; // Changed from window.screenY
-
-  sections.forEach((sec) => {
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
-        links.classList.remove("active");
-      });
-
-      // Use document.querySelector to find the navigation link with the corresponding href
-      let link = document.querySelector(
-        ".navigation-bar a[href='#" + id + "']"
-      );
-
-      if (link) {
-        link.classList.add("active");
-      }
-    }
-  });
+let content = document.querySelector(".content");
+let lastsection = document.getElementById("contacts");
+let rect = lastsection.getBoundingClientRect();
+content.onscroll = () => {
+  console.log(content.offsetTop);
 };
+
+// window.onscroll = () => {
+//   // Use window.scrollY to get the current scroll position
+//   let top = window.scrollY; // Changed from window.screenY
+
+//   sections.forEach((sec) => {
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       navlinks.forEach((links) => {
+//         links.classList.remove("active");
+//       });
+
+//       // Use document.querySelector to find the navigation link with the corresponding href
+//       let link = document.querySelector(
+//         ".navigation-bar a[href='#" + id + "']"
+//       );
+
+//       if (link) {
+//         link.classList.add("active");
+//       }
+//     }
+//   });
+// };
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "contactaniketraut@gmail.com",
+    Password: "1BF2286851C0A7F08FB815B5B3946D8CE195",
+    To: "rautnaniket@gmail.com",
+    From: "contactaniketraut@gmail.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
