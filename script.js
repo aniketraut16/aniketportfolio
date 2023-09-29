e596b5b66dedee1d7c495c5839f2ae08a8aefce1
Aniket <rautan_1@rknec.edu>
2023-09-29 12:30:35 +0530

    commit

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ var typed = new Typed(".auto-type", {
   loop: true,
 });
 
-function sendEmail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "contactaniketraut@gmail.com",
-    Password: "1BF2286851C0A7F08FB815B5B3946D8CE195",
-    To: "rautnaniket@gmail.com",
-    From: "contactaniketraut@gmail.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
-}
-
 const allSections = document.querySelectorAll("section");
 if (document.querySelector(".content").clientWidth > 500) {
   const observer = new IntersectionObserver((entries) => {
